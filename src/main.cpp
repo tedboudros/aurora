5a91125997769f28af818a044523630754bd582b
Ted Boudros <theodore.boudros.design@gmail.com>
2021-03-07 18:03:35 +0200

    [feat] Setup sublime project.

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char* args[]) {
+	std::cout << "Hello there" << std::endl;
+	std::cin.get();
+	return 0;
+} 
