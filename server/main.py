b123da11ee0654bbb00e8f3694cf3e216d672037
Ted Boudros <theodore.boudros.design@gmail.com>
2021-08-05 00:00:20 +0300

    [feat] Make steam games into a simple http server

--- server/main.py
+++ server/main.py
@@ -1,4 +1,26 @@
 from Integrations.steam import get_all_steam_games_from_filesystem
+from wsgiref.simple_server import make_server
 
-all_games = get_all_steam_games_from_filesystem()
-print(all_games)
+import falcon
+
+
+class SteamGamesResource:
+    def on_get(self, req, resp):
+
+        steam_games = get_all_steam_games_from_filesystem();
+
+        resp.status = falcon.HTTP_200
+        resp.media = steam_games
+
+
+app = falcon.App()
+
+steam = SteamGamesResource()
+app.add_route('/steam', steam)
+
+if __name__ == '__main__':
+    with make_server('', 8000, app) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
